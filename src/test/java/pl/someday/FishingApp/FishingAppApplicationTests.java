43b55414bc222aa23545ee4d43fc9fdca985c995
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-09-23 13:51:48 +0200

    Initial commit

---
+++ src/test/java/pl/someday/FishingApp/FishingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.someday.FishingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
