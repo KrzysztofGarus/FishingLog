d9f6e12893ef9a3399f3bfc8a7afd6c25ba5c5cd
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-01 16:16:03 +0200

    added UserDetailsServiceImpl

---
+++ src/main/java/pl/someday/FishingApp/user/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package pl.someday.FishingApp.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO throw if not exist
+        return userRepository.findByUsername(username);
+    }
+}
