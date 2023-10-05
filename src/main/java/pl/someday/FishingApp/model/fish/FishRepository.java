1b6570c9169498bebc5d7b473000b87b17364399
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-05 22:28:50 +0200

    added methods to FishRepository

--- src/main/java/pl/someday/FishingApp/model/fish/FishRepository.java
+++ src/main/java/pl/someday/FishingApp/model/fish/FishRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FishRepository extends JpaRepository<Fish, Long> {
 
+    Fish getFishById(Long id);
+    void delete(Fish fish);
+
 }
