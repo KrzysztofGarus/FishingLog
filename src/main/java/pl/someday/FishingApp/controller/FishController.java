e09ed581e1a691922d416ef5e4837bc5401f9e8a
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-07 11:49:09 +0200

    updated FishController - added FishNameRepository and used it in /add mapping

--- src/main/java/pl/someday/FishingApp/controller/FishController.java
+++ src/main/java/pl/someday/FishingApp/controller/FishController.java
@@ -9,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.someday.FishingApp.model.fish.Fish;
 import pl.someday.FishingApp.model.fish.FishRepository;
-import pl.someday.FishingApp.model.fishingSession.FishingSession;
+import pl.someday.FishingApp.model.fishName.FishNameRepository;
 import pl.someday.FishingApp.model.fishingSession.FishingSessionRepository;
 
 @Controller
 @RequestMapping("/user/fish")
 public class FishController {
+
+    private final FishNameRepository fishNameRepository;
     private final FishRepository fishRepository;
     private final FishingSessionRepository fishingSessionRepository;
     @Autowired
-    public FishController(FishRepository fishRepository, FishingSessionRepository fishingSessionRepository) {
+    public FishController(FishNameRepository fishNameRepository, FishRepository fishRepository, FishingSessionRepository fishingSessionRepository) {
+        this.fishNameRepository = fishNameRepository;
         this.fishRepository = fishRepository;
         this.fishingSessionRepository = fishingSessionRepository;
     }
@@ -27,12 +30,14 @@ public class FishController {
     public String addFish(@RequestParam Long sessionId, Model model){
         Fish fish = new Fish();
         fish.setFishingSession(fishingSessionRepository.getFishingSessionById(sessionId));
+        model.addAttribute("fishNames", fishNameRepository.findAll());
+        System.out.println("fishNames: " + model.getAttribute("fishNames"));
         model.addAttribute("fish", fish);
         return "/user/fish/fish-add";
     }
 
     @PostMapping("/add")
-    public String addFishtoSession(Fish fish){
+    public String addFishToSession(Fish fish){
         fishRepository.save(fish);
         return "redirect:/user/session/all";
     }
