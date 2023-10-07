d28e872924f5951b9280c92d040dc5d7387cd54b
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-07 11:53:17 +0200

    added repository for fishname

---
+++ src/main/java/pl/someday/FishingApp/model/fishName/FishNameRepository.java
@@ -0,0 +1,11 @@
+package pl.someday.FishingApp.model.fishName;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.someday.FishingApp.model.fishingSpot.FishingSpot;
+
+@Repository
+public interface FishNameRepository extends JpaRepository<FishName, Long> {
+
+}
+
