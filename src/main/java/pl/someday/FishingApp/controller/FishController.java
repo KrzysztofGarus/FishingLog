225894937d76676eddef832f3a54b065ecc002c5
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-07 15:36:53 +0200

    added delete and update methods

--- src/main/java/pl/someday/FishingApp/controller/FishController.java
+++ src/main/java/pl/someday/FishingApp/controller/FishController.java
@@ -3,6 +3,7 @@ package pl.someday.FishingApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import pl.someday.FishingApp.model.fish.Fish;
 import pl.someday.FishingApp.model.fish.FishRepository;
 import pl.someday.FishingApp.model.fishName.FishNameRepository;
+import pl.someday.FishingApp.model.fishingSession.FishingSession;
 import pl.someday.FishingApp.model.fishingSession.FishingSessionRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user/fish")
@@ -31,7 +35,6 @@ public class FishController {
         Fish fish = new Fish();
         fish.setFishingSession(fishingSessionRepository.getFishingSessionById(sessionId));
         model.addAttribute("fishNames", fishNameRepository.findAll());
-        System.out.println("fishNames: " + model.getAttribute("fishNames"));
         model.addAttribute("fish", fish);
         return "/user/fish/fish-add";
     }
@@ -42,7 +45,38 @@ public class FishController {
         return "redirect:/user/session/all";
     }
 
+    @GetMapping("/update")
+    public String editFish(@RequestParam Long id, Model model) {
+        Fish fish = fishRepository.getFishById(id);
+        model.addAttribute("fish", fish);
+        model.addAttribute("fishNames", fishNameRepository.findAll());
+        return "/user/fish/fish-update";
+    }
 
+    @PostMapping("/update")
+    public String editFish(@RequestParam Long id, @ModelAttribute("fish") Fish updatedFish){
+        Fish fish = fishRepository.getFishById(id);
+        fish.setFishName(updatedFish.getFishName());
+        fish.setWeight(updatedFish.getWeight());
+        fish.setLength(updatedFish.getLength());
+        fishRepository.save(fish);
+        return "redirect:/user/session/all";
+    }
+
+    @GetMapping("/delete")
+        public String deleteFish(@RequestParam Long id, Model model){
+            model.addAttribute("fish", fishRepository.getFishById(id));
+            return "/user/fish/fish-delete";
+        }
+    @PostMapping("/delete")
+    public String deleteFish(@RequestParam Long fishId, @RequestParam Long sessionId) {
+        FishingSession fishingSession = fishingSessionRepository.getFishingSessionById(sessionId);
+        List<Fish> fishList = fishingSession.getFishList();
+        fishList.removeIf(fish -> fish.getId().equals(fishId));
+        fishingSession.setFishList(fishList);
+        fishingSessionRepository.save(fishingSession);
+        return "redirect:/user/session/all";
+    }
 }
 
 
