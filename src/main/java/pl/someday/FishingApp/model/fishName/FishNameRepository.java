1a953dae1871acb8b858647ff6d59670ea4abb61
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-08 11:15:30 +0200

    added method in repo

--- src/main/java/pl/someday/FishingApp/model/fishName/FishNameRepository.java
+++ src/main/java/pl/someday/FishingApp/model/fishName/FishNameRepository.java
@@ -2,10 +2,13 @@ package pl.someday.FishingApp.model.fishName;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.someday.FishingApp.model.fishingSession.FishingSession;
 import pl.someday.FishingApp.model.fishingSpot.FishingSpot;
 
 @Repository
 public interface FishNameRepository extends JpaRepository<FishName, Long> {
+
+    FishName getFishNameById(Long id);
 
 }
 
