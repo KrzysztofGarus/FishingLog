5c62c2df92ca9231a96ef91c00e21072ff69de22
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-09 00:02:24 +0200

    added method to repo

--- src/main/java/pl/someday/FishingApp/model/fishingSession/FishingSessionRepository.java
+++ src/main/java/pl/someday/FishingApp/model/fishingSession/FishingSessionRepository.java
@@ -1,10 +1,14 @@
 package pl.someday.FishingApp.model.fishingSession;
 
+import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pl.someday.FishingApp.dto.FishCountForSpotDTO;
 
+import java.lang.annotation.Native;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface FishingSessionRepository extends JpaRepository<FishingSession, Long> {
@@ -14,5 +18,18 @@ public interface FishingSessionRepository extends JpaRepository<FishingSession, 
     FishingSession getFishingSessionById(Long id);
 
     void delete(FishingSession fishingSession);
+
+    @Query("SELECT NEW pl.someday.FishingApp.dto.FishCountForSpotDTO(fn.name, COUNT(fn.name)) " +
+            "FROM FishingSession fs " +
+            "JOIN Fish f ON fs.id = f.fishingSession.id " +
+            "JOIN FishName fn ON f.fishName.id = fn.id " +
+            "JOIN fs.fishingSpot spot " +
+            "WHERE spot.id = :spotId " +
+            "GROUP BY fn.name")
+    List<FishCountForSpotDTO> getFishCountsForSpot(@Param("spotId") Long spotId);
+
+
+
+
 }
 
