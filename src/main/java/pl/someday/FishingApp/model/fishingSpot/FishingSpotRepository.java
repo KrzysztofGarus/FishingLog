decf8edd3a071c79226ed7b70e8bc1c37684514a
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-09 00:02:18 +0200

    added method to repo

--- src/main/java/pl/someday/FishingApp/model/fishingSpot/FishingSpotRepository.java
+++ src/main/java/pl/someday/FishingApp/model/fishingSpot/FishingSpotRepository.java
@@ -3,7 +3,12 @@ package pl.someday.FishingApp.model.fishingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FishingSpotRepository extends JpaRepository<FishingSpot, Long> {
+
+    List<FishingSpot> findAllByOrderByName();
 
 }
