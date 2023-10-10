0c1778e02d722876a37c7026356019af39a1afb5
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-10 16:42:36 +0200

    updated date formatter

--- src/main/java/pl/someday/FishingApp/service/DateFormatterForDTO.java
+++ src/main/java/pl/someday/FishingApp/service/DateFormatterForDTO.java
@@ -4,19 +4,32 @@ import org.springframework.stereotype.Service;
 import pl.someday.FishingApp.dto.FishingSpotCalendarDTO;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DateFormatterForDTO {
 
-    public List<FishingSpotCalendarDTO> changeDateFormat(List<FishingSpotCalendarDTO> inputlist) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy, M, d");
-        for (FishingSpotCalendarDTO input : inputlist) {
-            Date inputDate = input.getDate();
-            String outputDate = sdf.format(inputDate);
-            input.setDateString(outputDate);
-        }
-        return inputlist;
+    public List<FishingSpotCalendarDTO> processDates(List<FishingSpotCalendarDTO> inputList) {
+        return inputList.stream()
+                .map(this::processDateInDTO)
+                .collect(Collectors.toList());
+    }
+
+    private FishingSpotCalendarDTO processDateInDTO(FishingSpotCalendarDTO dto) {
+        LocalDate date = dto.getDate();
+        String yearMonthDayString = convertDateToNumber(date);
+
+        return new FishingSpotCalendarDTO(date, dto.getCount(), yearMonthDayString);
+    }
+
+    private String convertDateToNumber(LocalDate date) {
+        int year = date.getYear();
+        int month = date.getMonthValue() - 1;
+        int day = date.getDayOfMonth();
+
+        return year + ", " + month + ", " + day;
     }
 }
