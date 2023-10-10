2b2760598201ba19c81a2c82e4f4c3bec8000923
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-10 20:33:59 +0200

    added sumWeight

--- src/main/java/pl/someday/FishingApp/dto/FishCountForSpotDTO.java
+++ src/main/java/pl/someday/FishingApp/dto/FishCountForSpotDTO.java
@@ -3,15 +3,19 @@ package pl.someday.FishingApp.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class FishCountForSpotDTO {
 
     private String name;
     private Long count;
+    private BigDecimal sumWeight;
 
-    public FishCountForSpotDTO(String name, Long count) {
+    public FishCountForSpotDTO(String name, Long count, BigDecimal sumWeight) {
         this.name = name;
         this.count = count;
+        this.sumWeight = sumWeight;
     }
 }
