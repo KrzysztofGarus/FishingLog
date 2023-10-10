f00dce9ed3a5950497b045a8695c425b35446d44
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-10 20:35:19 +0200

    added order by fish name

--- src/main/java/pl/someday/FishingApp/repository/FishingSessionRepository.java
+++ src/main/java/pl/someday/FishingApp/repository/FishingSessionRepository.java
@@ -19,14 +19,16 @@ public interface FishingSessionRepository extends JpaRepository<FishingSession, 
 
     void delete(FishingSession fishingSession);
 
-    @Query("SELECT NEW pl.someday.FishingApp.dto.FishCountForSpotDTO(fn.name, COUNT(fn.name)) " +
+    @Query("SELECT NEW pl.someday.FishingApp.dto.FishCountForSpotDTO(fn.name, COUNT(fn.name), SUM(f.weight)) " +
             "FROM FishingSession fs " +
             "JOIN Fish f ON fs.id = f.fishingSession.id " +
             "JOIN FishName fn ON f.fishName.id = fn.id " +
             "JOIN fs.fishingSpot spot " +
             "WHERE spot.id = :spotId " +
-            "GROUP BY fn.name")
-    List<FishCountForSpotDTO> getFishCountsForSpot(@Param("spotId") Long spotId);
+            "GROUP BY fn.name " +
+            "ORDER BY fn.name")
+    List<FishCountForSpotDTO> getFishCountsAndWeightForSpot(@Param("spotId") Long spotId);
+
 
     @Query("SELECT new pl.someday.FishingApp.dto.FishingSpotCalendarDTO(fs.date, COUNT(fs)) " +
             "FROM FishingSession fs " +
