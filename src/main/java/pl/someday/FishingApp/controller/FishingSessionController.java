77f390ff8aaaf3139046afa7f24e6068f801ca1f
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-10 20:35:03 +0200

    added initbinder to transfer html date format to  java.time.LocalDate;

--- src/main/java/pl/someday/FishingApp/controller/FishingSessionController.java
+++ src/main/java/pl/someday/FishingApp/controller/FishingSessionController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import pl.someday.FishingApp.model.FishingSession;
 import pl.someday.FishingApp.repository.FishingSessionRepository;
@@ -11,20 +12,31 @@ import pl.someday.FishingApp.repository.FishingSpotRepository;
 import pl.someday.FishingApp.model.User;
 import pl.someday.FishingApp.repository.UserRepository;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 @RequestMapping("/user/session")
 public class FishingSessionController {
 
-    private final UserRepository userRepository;
     private final FishingSessionRepository fishingSessionRepository;
     private final FishingSpotRepository fishingSpotRepository;
 
-
     @Autowired
     public FishingSessionController(UserRepository userRepository, FishingSessionRepository fishingSessionRepository, FishingSpotRepository fishingSpotRepository) {
-        this.userRepository = userRepository;
         this.fishingSessionRepository = fishingSessionRepository;
         this.fishingSpotRepository = fishingSpotRepository;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text, DateTimeFormatter.ISO_DATE));
+            }
+        });
     }
 
     @GetMapping("/add")
@@ -36,6 +48,7 @@ public class FishingSessionController {
 
     @PostMapping("/add")
     public String addSessionForUser(@AuthenticationPrincipal User user, FishingSession fishingSession){
+
         fishingSession.setUser(user);
         fishingSessionRepository.save(fishingSession);
         return "redirect:/user/session/all";
