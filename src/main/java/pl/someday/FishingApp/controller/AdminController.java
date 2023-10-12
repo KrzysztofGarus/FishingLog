1d056b1389f305e566cabc244d5801320d98963e
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-12 20:52:05 +0200

    added admin methods for edit fish and add spot

--- src/main/java/pl/someday/FishingApp/controller/AdminController.java
+++ src/main/java/pl/someday/FishingApp/controller/AdminController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pl.someday.FishingApp.model.Fish;
 import pl.someday.FishingApp.model.FishName;
+import pl.someday.FishingApp.model.FishingSpot;
 import pl.someday.FishingApp.repository.FishNameRepository;
 import pl.someday.FishingApp.repository.FishingSessionRepository;
 import pl.someday.FishingApp.repository.FishingSpotRepository;
@@ -35,13 +37,27 @@ public class AdminController {
     @GetMapping("/fish/list")
     public String showListOfFishNames(Model model){
         model.addAttribute("fishNameList", fishNameRepository.findAll());
-        return "/admin/fish-name-list";
+        return "admin/fish/fish-name-list";
+    }
+    @GetMapping("/fish/update")
+    public String editFish(@RequestParam Long id, Model model){
+        FishName fishName = fishNameRepository.getFishNameById(id);
+        model.addAttribute("fishName",fishName);
+        return "admin/fish/fish-update";
+    }
+
+    @PostMapping("/fish/update")
+    public String editFish(@RequestParam Long id, @ModelAttribute("fishName") FishName updatedFishName){
+        FishName fishName = fishNameRepository.getFishNameById(id);
+        fishName.setName(updatedFishName.getName());
+        fishNameRepository.save(fishName);
+        return "redirect:/admin/fish/list";
     }
 
     @GetMapping("/fish/add")
     public String addFish(Model model){
         model.addAttribute("fishName", new FishName());
-        return "/admin/fish-add";
+        return "admin/fish/fish-add";
     }
     @PostMapping("/fish/add")
 
@@ -53,7 +69,7 @@ public class AdminController {
     @GetMapping("/fish/delete")
     public String deleteFish(@RequestParam Long id, Model model){
         model.addAttribute("fishName", fishNameRepository.getFishNameById(id));
-        return "/admin/fish-delete";
+        return "admin/fish/fish-delete";
     }
     @PostMapping("/fish/delete")
     public String deleteFish(@RequestParam Long id){
@@ -65,7 +81,7 @@ public class AdminController {
     @GetMapping("/spot/list")
     public String showListOfFishingSpots(Model model){
         model.addAttribute("fishingSpotList", fishingSpotRepository.findAllByOrderByName());
-        return "/admin/spot-list";
+        return "admin/spot/spot-list";
     }
 
     @GetMapping("/spot/details")
@@ -73,7 +89,17 @@ public class AdminController {
         DateFormatterForDTO dto = new DateFormatterForDTO();
         model.addAttribute("fishNamesCount", fishingSessionRepository.getFishCountsAndWeightForSpot(id));
         model.addAttribute("SessionDateCount", dto.processDates(fishingSessionRepository.getSessionCountForSpotAndDate(id)));
-        return "/admin/spot-details";
+        return "admin/spot/spot-details";
+    }
+    @GetMapping("/spot/add")
+    public String addSpot(Model model){
+        model.addAttribute("fishingSpot", new FishingSpot());
+        return "admin/spot/spot-add";
+    }
+    @PostMapping ("/spot/add")
+    public  String addSpot(FishingSpot fishingSpot){
+        fishingSpotRepository.save(fishingSpot);
+        return "redirect:/admin/spot/list";
     }
 
 }
