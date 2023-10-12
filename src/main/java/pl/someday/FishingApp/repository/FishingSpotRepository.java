bf0d6e347704ff548492090c9e859dee8aa7720d
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-12 21:49:48 +0200

    added repo methods

--- src/main/java/pl/someday/FishingApp/repository/FishingSpotRepository.java
+++ src/main/java/pl/someday/FishingApp/repository/FishingSpotRepository.java
@@ -5,10 +5,17 @@ import org.springframework.stereotype.Repository;
 import pl.someday.FishingApp.model.FishingSpot;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Repository
 public interface FishingSpotRepository extends JpaRepository<FishingSpot, Long> {
 
     List<FishingSpot> findAllByOrderByName();
+    void delete(FishingSpot fishingSpot);
 
+    default FishingSpot findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Nie znaleziono Spotu o ID: " + id));
+    }
 }
