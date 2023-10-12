e0ccc135baef2bd6f6b77121b8264441058b0b33
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-12 21:49:24 +0200

    added spot delete and update mappings

--- src/main/java/pl/someday/FishingApp/controller/AdminController.java
+++ src/main/java/pl/someday/FishingApp/controller/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import pl.someday.FishingApp.model.Fish;
 import pl.someday.FishingApp.model.FishName;
 import pl.someday.FishingApp.model.FishingSpot;
 import pl.someday.FishingApp.repository.FishNameRepository;
@@ -37,13 +36,13 @@ public class AdminController {
     @GetMapping("/fish/list")
     public String showListOfFishNames(Model model){
         model.addAttribute("fishNameList", fishNameRepository.findAll());
-        return "admin/fish/fish-name-list";
+        return "/admin/fish/fish-name-list";
     }
     @GetMapping("/fish/update")
     public String editFish(@RequestParam Long id, Model model){
         FishName fishName = fishNameRepository.getFishNameById(id);
         model.addAttribute("fishName",fishName);
-        return "admin/fish/fish-update";
+        return "/admin/fish/fish-update";
     }
 
     @PostMapping("/fish/update")
@@ -57,7 +56,7 @@ public class AdminController {
     @GetMapping("/fish/add")
     public String addFish(Model model){
         model.addAttribute("fishName", new FishName());
-        return "admin/fish/fish-add";
+        return "/admin/fish/fish-add";
     }
     @PostMapping("/fish/add")
 
@@ -69,7 +68,7 @@ public class AdminController {
     @GetMapping("/fish/delete")
     public String deleteFish(@RequestParam Long id, Model model){
         model.addAttribute("fishName", fishNameRepository.getFishNameById(id));
-        return "admin/fish/fish-delete";
+        return "/admin/fish/fish-delete";
     }
     @PostMapping("/fish/delete")
     public String deleteFish(@RequestParam Long id){
@@ -81,7 +80,7 @@ public class AdminController {
     @GetMapping("/spot/list")
     public String showListOfFishingSpots(Model model){
         model.addAttribute("fishingSpotList", fishingSpotRepository.findAllByOrderByName());
-        return "admin/spot/spot-list";
+        return "/admin/spot/spot-list";
     }
 
     @GetMapping("/spot/details")
@@ -89,12 +88,12 @@ public class AdminController {
         DateFormatterForDTO dto = new DateFormatterForDTO();
         model.addAttribute("fishNamesCount", fishingSessionRepository.getFishCountsAndWeightForSpot(id));
         model.addAttribute("SessionDateCount", dto.processDates(fishingSessionRepository.getSessionCountForSpotAndDate(id)));
-        return "admin/spot/spot-details";
+        return "/admin/spot/spot-details";
     }
     @GetMapping("/spot/add")
     public String addSpot(Model model){
         model.addAttribute("fishingSpot", new FishingSpot());
-        return "admin/spot/spot-add";
+        return "/admin/spot/spot-add";
     }
     @PostMapping ("/spot/add")
     public  String addSpot(FishingSpot fishingSpot){
@@ -102,6 +101,32 @@ public class AdminController {
         return "redirect:/admin/spot/list";
     }
 
+    @GetMapping("/spot/delete")
+    public String deleteSpot(@RequestParam Long id, Model model){
+        FishingSpot fishingSpot = fishingSpotRepository.findByIdOrThrow(id);
+        model.addAttribute("fishingSpot", fishingSpot);
+        return "/admin/spot/spot-delete";
+    }
+
+    @PostMapping("/spot/delete")
+    public String deleteSpot(@RequestParam Long id){
+        FishingSpot fishingSpot = fishingSpotRepository.findByIdOrThrow(id);
+        fishingSpotRepository.delete(fishingSpot);
+        return "redirect:/admin/spot/list";
+    }
+    @GetMapping("/spot/update")
+    public String editSpot(@RequestParam Long id, Model model) {
+        model.addAttribute("fishingSpot", fishingSpotRepository.findByIdOrThrow(id));
+        return "/admin/spot/spot-update";
+    }
+
+    @PostMapping("/spot/update")
+    public String editSpot(@RequestParam Long id, @ModelAttribute("fishingSpot") FishingSpot updatedFishingSpot){
+        FishingSpot fishingSpot = fishingSpotRepository.findByIdOrThrow(id);
+        fishingSpot.setName(updatedFishingSpot.getName());
+        fishingSpotRepository.save(fishingSpot);
+        return "redirect:/admin/spot/list";
+    }
 }
 
 
