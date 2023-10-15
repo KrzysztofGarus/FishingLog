e217c58da3d0fefc3c3fa494451d2e694702210e
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-15 16:39:48 +0200

    added method to count active users

--- src/main/java/pl/someday/FishingApp/repository/UserRepository.java
+++ src/main/java/pl/someday/FishingApp/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package pl.someday.FishingApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.someday.FishingApp.model.User;
 
@@ -8,4 +9,7 @@ import pl.someday.FishingApp.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
+
+    @Query("SELECT COUNT(u) FROM User u WHERE u.active = true")
+    Long countActiveUsers();
 }
