3435c213f7315da16e8ab85ae81a565b0ab17d69
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-16 11:40:02 +0200

    updated register controller

--- src/main/java/pl/someday/FishingApp/controller/RegisterController.java
+++ src/main/java/pl/someday/FishingApp/controller/RegisterController.java
@@ -3,40 +3,46 @@ package pl.someday.FishingApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import pl.someday.FishingApp.model.User;
+import org.springframework.ui.Model;
+import pl.someday.FishingApp.dto.UserRegistrationDTO;
 import pl.someday.FishingApp.repository.UserRepository;
+import pl.someday.FishingApp.service.UserService;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class RegisterController {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     @Autowired
-    public RegisterController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public RegisterController(UserRepository userRepository, PasswordEncoder passwordEncoder, UserService userService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @GetMapping("/register")
-    public String showRegistrationPage() {
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("userDto", new UserRegistrationDTO());
         return "register";
+
+
     }
 
     @PostMapping("/register")
-    public String processRegistration(@RequestParam String name, @RequestParam String surname,
-                                      @RequestParam Long license, @RequestParam String username, @RequestParam String password) {
-        User user = new User();
-        user.setName(name);
-        user.setSurname(surname);
-        user.setLicense(license);
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.encode(password));
-        user.setRole("ROLE_USER"); // default USER role
-        userRepository.save(user);
+    public String processRegistration(@ModelAttribute("userDto") @Valid UserRegistrationDTO userDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        userService.registerNewUser(userDto);
         return "redirect:/login";
     }
 }
