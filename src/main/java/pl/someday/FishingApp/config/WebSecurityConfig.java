ab7f9a3f0704ae01fa924297ceb34a2c1a5a4686
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-16 11:55:30 +0200

    updated permits for register and homepage

--- src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
+++ src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
@@ -36,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
                 .antMatchers("/user/**").hasRole("USER")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
