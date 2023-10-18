a1b93a4a4b8900d19bf9e07de6d0cbc1db7b701f
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-18 17:15:46 +0200

    updated RegisterController

--- src/main/java/pl/someday/FishingApp/controller/RegisterController.java
+++ src/main/java/pl/someday/FishingApp/controller/RegisterController.java
@@ -1,7 +1,6 @@
 package pl.someday.FishingApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +8,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 import pl.someday.FishingApp.dto.UserRegistrationDTO;
-import pl.someday.FishingApp.repository.UserRepository;
 import pl.someday.FishingApp.service.UserService;
-
 import javax.validation.Valid;
 
 
 @Controller
 public class RegisterController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
     @Autowired
-    public RegisterController(UserRepository userRepository, PasswordEncoder passwordEncoder, UserService userService) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public RegisterController(UserService userService) {
         this.userService = userService;
     }
 
@@ -33,8 +26,6 @@ public class RegisterController {
     public String showRegistrationPage(Model model) {
         model.addAttribute("userDto", new UserRegistrationDTO());
         return "register";
-
-
     }
 
     @PostMapping("/register")
