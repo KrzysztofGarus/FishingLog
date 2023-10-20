3057fa85c20b0b9f086dd84ec2fd48f5ab8971d1
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 11:36:10 +0200

    added doc to class

--- src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
+++ src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
@@ -14,25 +14,52 @@ import pl.someday.FishingApp.service.UserDetailsServiceImpl;
 
 import java.util.Collection;
 
+/**
+ * Konfiguracja zabezpieczeń aplikacji przy użyciu Spring Security.
+ * Ta klasa rozszerza konfigurację domyślną klasy `WebSecurityConfigurerAdapter` i dostarcza
+ * ustawienia dotyczące autentykacji, uprawnień dostępu i formularza logowania.
+ */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailsService;
 
+    /**
+     * Konstruktor klasy `WebSecurityConfig`.
+     *
+     * @param userDetailsService Serwis obsługujący szczegóły użytkowników.
+     */
     public WebSecurityConfig(UserDetailsServiceImpl userDetailsService){
         this.userDetailsService = userDetailsService;
     }
 
+    /**
+     * Konfiguracja szyfrowania hasła.
+     *
+     * @return Implementacja `PasswordEncoder`.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    /**
+     * Konfiguracja menedżera autentykacji.
+     *
+     * @param auth Menedżer autentykacji Spring Security.
+     * @throws Exception Wyrzucane w przypadku błędów konfiguracji autentykacji.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
+    /**
+     * Konfiguracja zabezpieczeń HTTP.
+     *
+     * @param http Konfigurator obiektu HttpSecurity.
+     * @throws Exception Wyrzucane w przypadku błędów konfiguracji zabezpieczeń HTTP.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
