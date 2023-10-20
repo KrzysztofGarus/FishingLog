6cf85e5c67a4baf4758744f0e20bf4f56de9d458
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 11:37:48 +0200

    added doc to class

--- src/main/java/pl/someday/FishingApp/controller/RegisterController.java
+++ src/main/java/pl/someday/FishingApp/controller/RegisterController.java
@@ -12,22 +12,46 @@ import pl.someday.FishingApp.service.UserService;
 import javax.validation.Valid;
 
 
+/**
+ * Kontroler obsługujący proces rejestracji użytkowników.
+ * Klasa ta jest odpowiedzialna za obsługę żądań związanych z rejestracją nowych użytkowników,
+ * prezentację widoku rejestracji oraz przetwarzanie danych wprowadzonych przez użytkownika
+ * podczas rejestracji.
+ */
 @Controller
 public class RegisterController {
 
     private final UserService userService;
 
+    /**
+     * Konstruktor klasy `RegisterController`.
+     *
+     * @param userService Serwis obsługujący operacje związane z użytkownikami.
+     */
     @Autowired
     public RegisterController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia strony rejestracji.
+     *
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku strony rejestracji.
+     */
     @GetMapping("/register")
     public String showRegistrationPage(Model model) {
         model.addAttribute("userDto", new UserRegistrationDTO());
         return "register";
     }
 
+    /**
+     * Metoda obsługująca proces rejestracji nowego użytkownika.
+     *
+     * @param userDto Obiekt DTO (Data Transfer Object) zawierający dane nowego użytkownika.
+     * @param result Obiekt `BindingResult` do przechwytywania błędów walidacji.
+     * @return Nazwa widoku strony rejestracji w przypadku błędów, w przeciwnym razie przekierowanie na stronę logowania.
+     */
     @PostMapping("/register")
     public String processRegistration(@ModelAttribute("userDto") @Valid UserRegistrationDTO userDto, BindingResult result) {
         if (result.hasErrors()) {
