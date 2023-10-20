db41a808ac324551559df839383565375dbf1a7b
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 12:31:48 +0200

    added doc to service

--- src/main/java/pl/someday/FishingApp/service/UserDetailsServiceImpl.java
+++ src/main/java/pl/someday/FishingApp/service/UserDetailsServiceImpl.java
@@ -7,13 +7,29 @@ import org.springframework.stereotype.Service;
 import pl.someday.FishingApp.model.User;
 import pl.someday.FishingApp.repository.UserRepository;
 
+/**
+ * Implementacja interfejsu UserDetailsService obsługująca pobieranie szczegółów użytkownika.
+ */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
+
+    /**
+     * Konstruktor klasy UserDetailsServiceImpl.
+     *
+     * @param userRepository Repozytorium użytkowników.
+     */
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Pobiera szczegóły użytkownika na podstawie nazwy użytkownika.
+     *
+     * @param username Nazwa użytkownika.
+     * @return Szczegóły użytkownika (implementacja UserDetails).
+     * @throws UsernameNotFoundException Wyjątek w przypadku, gdy nie znaleziono użytkownika o podanej nazwie.
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
