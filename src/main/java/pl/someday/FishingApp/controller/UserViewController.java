c5344091a9c7b306b1b75000591b0db2b8d28a16
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 11:55:47 +0200

    added doc to class

--- src/main/java/pl/someday/FishingApp/controller/UserViewController.java
+++ src/main/java/pl/someday/FishingApp/controller/UserViewController.java
@@ -17,6 +17,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+/**
+ * Kontroler obsługujący interakcje użytkownika w ramach funkcji przypisanych do roli "USER".
+ * Udostępnia operacje takie jak przeglądanie statystyk dotyczących sesji wędkarskich,
+ * dodawanie i edycja informacji o złowionych rybach oraz zarządzanie sesjami wędkarskimi.
+ */
 @Controller
 @RequestMapping("/user")
 public class UserViewController {
@@ -25,6 +30,14 @@ public class UserViewController {
     private final FishNameRepository fishNameRepository;
     private final FishRepository fishRepository;
 
+    /**
+     * Konstruktor kontrolera, wstrzykujący zależności do repozytoriów.
+     *
+     * @param fishingSessionRepository Repozytorium sesji wędkarskich.
+     * @param fishingSpotRepository    Repozytorium miejsc wędkowania.
+     * @param fishNameRepository       Repozytorium nazw ryb.
+     * @param fishRepository           Repozytorium danych o rybach.
+     */
     @Autowired
     public UserViewController(FishingSessionRepository fishingSessionRepository, FishingSpotRepository fishingSpotRepository, FishNameRepository fishNameRepository, FishRepository fishRepository) {
         this.fishingSessionRepository = fishingSessionRepository;
@@ -33,6 +46,13 @@ public class UserViewController {
         this.fishRepository = fishRepository;
     }
 
+    /**
+     * Metoda inicjalizująca binder dla kontrolera.
+     * Binder jest używany do konfiguracji przekształceń danych wejściowych,
+     * takich jak konwersja tekstu na datę.
+     *
+     * @param binder Obiekt WebDataBinder, używany do konfiguracji przekształceń danych wejściowych.
+     */
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
@@ -43,6 +63,15 @@ public class UserViewController {
         });
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia panelu użytkownika (dashboard).
+     * Wyświetla statystyki dotyczące sesji wędkarskich, takie jak liczba sesji, popularne miejsce,
+     * maksymalna długość i waga złowionych ryb.
+     *
+     * @param user  Zalogowany użytkownik.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku panelu użytkownika.
+     */
     @GetMapping("/dashboard")
     public String showUserDashboard(@AuthenticationPrincipal User user, Model model){
         DateFormatterForDTO dto = new DateFormatterForDTO();
@@ -56,11 +85,23 @@ public class UserViewController {
         return "/user/dashboard";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia mapy.
+     *
+     * @return Nazwa widoku mapy.
+     */
     @GetMapping("/maps")
     public String showMapForSelectedID(){
         return "/user/maps";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza dodawania nowego zarejestrowanego rybu do sesji wędkarskiej.
+     *
+     * @param sessionId Identyfikator sesji wędkarskiej.
+     * @param model     Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza dodawania ryby.
+     */
     @GetMapping("/fish/add")
     public String addFish(@RequestParam Long sessionId, Model model){
         Fish fish = new Fish();
@@ -70,12 +111,25 @@ public class UserViewController {
         return "user/fish-add";
     }
 
+    /**
+     * Metoda obsługująca proces dodawania nowego zarejestrowanego rybu do sesji wędkarskiej.
+     *
+     * @param fish Nowy ryba do dodania.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/fish/add")
     public String addFishToSession(Fish fish){
         fishRepository.save(fish);
         return "redirect:/user/session/list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza edycji informacji o zarejestrowanym rybie.
+     *
+     * @param id    Identyfikator ryby do edycji.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza edycji ryby.
+     */
     @GetMapping("/fish/update")
     public String editFish(@RequestParam Long id, Model model) {
         Fish fish = fishRepository.getFishById(id);
@@ -84,6 +138,13 @@ public class UserViewController {
         return "user/fish-update";
     }
 
+    /**
+     * Metoda obsługująca proces edycji informacji o zarejestrowanym rybie.
+     *
+     * @param id          Identyfikator ryby do edycji.
+     * @param updatedFish Zaktualizowana informacja o rybie.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/fish/update")
     public String editFish(@RequestParam Long id, @ModelAttribute("fish") Fish updatedFish){
         Fish fish = fishRepository.getFishById(id);
@@ -94,11 +155,26 @@ public class UserViewController {
         return "redirect:/user/session/list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza potwierdzenia usunięcia informacji o zarejestrowanym rybie.
+     *
+     * @param id    Identyfikator ryby do usunięcia.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza potwierdzenia usunięcia ryby.
+     */
     @GetMapping("/fish/delete")
     public String deleteFish(@RequestParam Long id, Model model){
         model.addAttribute("fish", fishRepository.getFishById(id));
         return "user/fish-delete";
     }
+
+    /**
+     * Metoda obsługująca proces usunięcia informacji o zarejestrowanym rybie z sesji wędkarskiej.
+     *
+     * @param fishId    Identyfikator ryby do usunięcia.
+     * @param sessionId Identyfikator sesji wędkarskiej, z której usuwana jest ryba.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/fish/delete")
     public String deleteFish(@RequestParam Long fishId, @RequestParam Long sessionId) {
         FishingSession fishingSession = fishingSessionRepository.getFishingSessionById(sessionId);
@@ -109,6 +185,12 @@ public class UserViewController {
         return "redirect:/user/session/list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza dodawania nowej sesji wędkarskiej.
+     *
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza dodawania sesji wędkarskiej.
+     */
     @GetMapping("/session/add")
     public String addSession(Model model){
         model.addAttribute("fishingSession", new FishingSession());
@@ -116,6 +198,13 @@ public class UserViewController {
         return "/user/session-add";
     }
 
+    /**
+     * Metoda obsługująca proces dodawania nowej sesji wędkarskiej dla zalogowanego użytkownika.
+     *
+     * @param user           Zalogowany użytkownik.
+     * @param fishingSession Nowa sesja wędkarska do dodania.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/session/add")
     public String addSessionForUser(@AuthenticationPrincipal User user, FishingSession fishingSession){
         fishingSession.setUser(user);
@@ -123,11 +212,24 @@ public class UserViewController {
         return "redirect:/user/session/list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza potwierdzenia usunięcia sesji wędkarskiej.
+     *
+     * @param id    Identyfikator sesji wędkarskiej do usunięcia.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza potwierdzenia usunięcia sesji wędkarskiej.
+     */
     @GetMapping("/session/delete")
     public String deleteSessionID(@RequestParam Long id, Model model){
         model.addAttribute("fishingSession", fishingSessionRepository.getFishingSessionById(id));
         return "/user/session-delete";
     }
+    /**
+     * Metoda obsługująca proces usunięcia sesji wędkarskiej dla zalogowanego użytkownika.
+     *
+     * @param id Identyfikator sesji wędkarskiej do usunięcia.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/session/delete")
     public String deleteSession(@RequestParam Long id) {
         FishingSession fishingSession = fishingSessionRepository.getFishingSessionById(id);
@@ -135,12 +237,26 @@ public class UserViewController {
         return "redirect:/user/session/list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia listy sesji wędkarskich użytkownika.
+     *
+     * @param user  Zalogowany użytkownik.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku listy sesji wędkarskich użytkownika.
+     */
     @GetMapping("/session/list")
     public String showUserSessions(@AuthenticationPrincipal User user, Model model){
         model.addAttribute("fishingSessions", fishingSessionRepository.findByUserUsernameOrderByDateDesc(user.getUsername()));
         return "user/session-list";
     }
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia formularza edycji istniejącej sesji wędkarskiej.
+     *
+     * @param id    Identyfikator sesji wędkarskiej do edycji.
+     * @param model Model Spring, używany do przekazywania danych do widoku.
+     * @return Nazwa widoku formularza edycji sesji wędkarskiej.
+     */
     @GetMapping("/session/update")
     public String editSession(@RequestParam Long id, Model model){
         FishingSession fishingSession = fishingSessionRepository.getFishingSessionById(id);
@@ -149,6 +265,13 @@ public class UserViewController {
         return "/user/session-update";
     }
 
+    /**
+     * Metoda obsługująca proces edycji sesji wędkarskiej dla zalogowanego użytkownika.
+     *
+     * @param id            Identyfikator sesji wędkarskiej do edycji.
+     * @param updatedSession Zaktualizowane informacje o sesji wędkarskiej.
+     * @return Przekierowanie na widok listy sesji wędkarskich.
+     */
     @PostMapping("/session/update")
     public String editSession(@RequestParam Long id, @ModelAttribute("fishingSession") FishingSession updatedSession) {
         FishingSession fishingSession = fishingSessionRepository.getFishingSessionById(id);
