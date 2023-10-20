065fa649135bf8902c5eed95239da4b7660373c6
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 12:32:50 +0200

    added doc to service

--- src/main/java/pl/someday/FishingApp/service/UserService.java
+++ src/main/java/pl/someday/FishingApp/service/UserService.java
@@ -7,18 +7,33 @@ import pl.someday.FishingApp.dto.UserRegistrationDTO;
 import pl.someday.FishingApp.model.User;
 import pl.someday.FishingApp.repository.UserRepository;
 
+/**
+ * Serwis obsługujący operacje związane z użytkownikami.
+ */
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Konstruktor klasy UserService.
+     *
+     * @param userRepository   Repozytorium użytkowników.
+     * @param passwordEncoder  Koder hasła.
+     */
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Rejestruje nowego użytkownika na podstawie danych z obiektu DTO.
+     *
+     * @param userDto Obiekt DTO zawierający dane nowego użytkownika.
+     * @return Zarejestrowany użytkownik.
+     */
     public User registerNewUser(UserRegistrationDTO userDto) {
         User user = new User();
         user.setName(userDto.getName());
