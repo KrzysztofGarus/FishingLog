6490cfa52a9a1841d3478cb566bd04a01cd7beb6
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 12:24:55 +0200

    added doc to interface

--- src/main/java/pl/someday/FishingApp/repository/FishNameRepository.java
+++ src/main/java/pl/someday/FishingApp/repository/FishNameRepository.java
@@ -3,11 +3,27 @@ package pl.someday.FishingApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.someday.FishingApp.model.FishName;
+import pl.someday.FishingApp.model.FishingSpot;
 
+import java.util.NoSuchElementException;
+
+/**
+ * Interfejs repozytorium, który umożliwia dostęp do danych związanych z nazwami ryb.
+ */
 @Repository
 public interface FishNameRepository extends JpaRepository<FishName, Long> {
 
-    FishName getFishNameById(Long id);
+    /**
+     * Pobiera obiekt FishName na podstawie jego identyfikatora lub wyrzuca wyjątek, jeśli nie zostanie znaleziony.
+     *
+     * @param id Identyfikator FishName do pobrania.
+     * @return Znaleziony obiekt FishName.
+     * @throws NoSuchElementException jeśli nie zostanie znaleziony obiekt FishName o podanym identyfikatorze.
+     */
+    default FishName findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Nie znaleziono Ryby o ID: " + id));
+    }
 
 }
 
