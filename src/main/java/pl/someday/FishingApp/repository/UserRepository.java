0e32a0578306ba2b5a930aec20773ba9a3812ffa
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 12:29:37 +0200

    added doc to interface

--- src/main/java/pl/someday/FishingApp/repository/UserRepository.java
+++ src/main/java/pl/someday/FishingApp/repository/UserRepository.java
@@ -5,11 +5,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.someday.FishingApp.model.User;
 
+/**
+ * Interfejs repozytorium do obsługi operacji bazodanowych na encji User.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Pobiera obiekt User na podstawie nazwy użytkownika.
+     *
+     * @param username Nazwa użytkownika.
+     * @return Znaleziony obiekt User.
+     */
     User findByUsername(String username);
 
+    /**
+     * Zlicza aktywnych użytkowników w bazie danych.
+     *
+     * @return Liczba aktywnych użytkowników.
+     */
     @Query("SELECT COUNT(u) FROM User u WHERE u.active = true")
     Long countActiveUsers();
 }
