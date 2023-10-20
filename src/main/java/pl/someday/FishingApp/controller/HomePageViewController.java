d3f2d51d448c43796254d8c10d1ad6d608b3aa72
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-10-20 11:38:53 +0200

    added doc to class

--- src/main/java/pl/someday/FishingApp/controller/HomePageViewController.java
+++ src/main/java/pl/someday/FishingApp/controller/HomePageViewController.java
@@ -3,9 +3,19 @@ package pl.someday.FishingApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Kontroler obsługujący widok strony głównej.
+ * Klasa ta jest odpowiedzialna za obsługę żądania dotyczącego wyświetlenia strony głównej,
+ * które jest mapowane na adres URL "/".
+ */
 @Controller
 public class HomePageViewController {
 
+    /**
+     * Metoda obsługująca żądanie wyświetlenia strony głównej.
+     *
+     * @return Nazwa widoku strony głównej.
+     */
     @GetMapping("/")
     public String home() {
         return "home";
