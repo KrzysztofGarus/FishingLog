269034814b04a157cbb8878f2cdea251f3746078
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-11-18 19:00:13 +0100

    updated user redirect after login method

--- src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
+++ src/main/java/pl/someday/FishingApp/config/WebSecurityConfig.java
@@ -3,16 +3,17 @@ package pl.someday.FishingApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import pl.someday.FishingApp.service.UserDetailsServiceImpl;
 
-import java.util.Collection;
 
 /**
  * Konfiguracja zabezpieczeń aplikacji przy użyciu Spring Security.
@@ -43,12 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public AuthorizationManager<Authentication> authorityAuthorizationManager() {
+        return AuthorityAuthorizationManager.hasRole("ADMIN");
+    }
+
     /**
      * Konfiguracja menedżera autentykacji.
      *
      * @param auth Menedżer autentykacji Spring Security.
      * @throws Exception Wyrzucane w przypadku błędów konfiguracji autentykacji.
      */
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
@@ -62,6 +69,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        AuthorizationManager<Authentication> authorityAuthorizationManager = authorityAuthorizationManager();
+
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/register").permitAll()
@@ -71,14 +81,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .successHandler((request, response, authentication) -> {
-                    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-                    for (GrantedAuthority authority : authorities) {
-                        // TODO AuthorityAuthorizationManager.hasRole("ADMIN")
-                        if ("ROLE_ADMIN".equals(authority.getAuthority())) {
-                            response.sendRedirect("/admin/dashboard");
-                        } else {
-                            response.sendRedirect("/user/dashboard");
-                        }
+                    boolean isAdmin = authorityAuthorizationManager.check(() -> authentication, null).isGranted();
+                    if (isAdmin) {
+                        response.sendRedirect("/admin/dashboard");
+                    } else {
+                        response.sendRedirect("/user/dashboard");
                     }
                 })
                 .and()
